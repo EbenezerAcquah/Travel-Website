af8df9f921001a17110a75c75d3b23218ccf4095
Ebenezer Acquah <ebenezeracquah000@gmail.com>
2025-09-02 22:21:15 -0700

    Third Commit

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 let searchBtn = document.querySelector('#search-btn');
+let searchBar = document.querySelector('.search-bar-container');
+let formBtn = document.querySelector('#logn-btn');
+let loginForm= document.querySelector('.login-form-container');
+let formClose= document.querySelector('#form-close');
+let menu =document.querySelector('#menu-bar');
+let navbar =document.querySelector('.navbar');
